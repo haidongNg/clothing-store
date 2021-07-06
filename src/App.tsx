3b4293d600918cc4cc1376aab3d9a1de84cdd8af
haidongng <nguyenhaidong1412@gmail.com>
2021-07-06 21:22:56 +0700

    update layout home

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch';
 import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons/faShoppingCart';
 import image17 from '../src/assets/image17.png';
+import image20 from '../src/assets/image20.png';
 const App: FC = () => {
   return (
-    <div>
+    <div style={{ backgroundColor: '#F2F2F2' }}>
       <nav className="navbar is-transparent">
         <div className="navbar-brand">
           <a href="#a" className="navbar-item">
@@ -79,7 +80,7 @@ const App: FC = () => {
 
       {/* Collection */}
       <section className="section">
-        <div className="container">
+        <div className="ct-mw-container">
           <div className="columns">
             <div className="column">
               <div className="ct-collection ct-collection-image-1 p-6">
@@ -116,7 +117,180 @@ const App: FC = () => {
           </div>
         </div>
       </section>
+      {/* slideshow */}
+      <div className="section section--slideshow">
+        <div className="slideshow-container">
+          <div className="mySlides fade">
+            {/* <div className="numbertext">1 / 3</div>
+            <img src={image17} alt="" />
+            <div className="text">Caption Text</div> */}
 
+
+            <div className="is-flex is-flex-direction-row is-justify-content-space-around is-flex-wrap-nowrap">
+              <div className="p-2">
+                <figure className="image">
+                  <img src={image17} alt="" />
+                </figure>
+                {/* <div className="numbertext">1 / 3</div>
+                <img src={image17} alt="" />
+                <div className="text">Caption Text</div> */}
+              </div>
+              <div className="p-2">
+                <figure className="image">
+                  <img src={image17} alt="" />
+                </figure>
+              </div>
+              <div className="p-2">
+                <figure className="image">
+                  <img src={image17} alt="" />
+                </figure>
+              </div>
+              <div className="p-2">
+                <figure className="image">
+                  <img src={image17} alt="" />
+                </figure>
+              </div>
+            </div>
+          </div>
+          <div className="mySlides fade">
+            {/* <div className="numbertext">1 / 3</div>
+            <img src={image17} alt="" />
+            <div className="text">Caption Text</div> */}
+          </div>
+          <a href="#a" className="prev">&#10094;</a>
+          <a href="#a" className="next" >&#10095;</a>
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          <span className="dot"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+        </div>
+      </div>
+
+
+      {/*  */}
+      <div className="section">
+        <div className="columns is-multiline columns--your-my-like">
+          <div className="column is-3">
+            <div className="card card-ct-product">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src={image20} alt="Placeholder" />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="content">
+                  <h1 className="title">Women's blackvest Gucci</h1>
+                </div>
+              </div>
+              <footer className="card-footer card-footer-ct-product">
+                <a href="#a" className="card-footer-item">
+                  <h1 className="title is-4">$715</h1>
+                </a>
+                <a href="#a" className="card-footer-item">Show now</a>
+              </footer>
+            </div>
+          </div>
+          <div className="column is-3">
+            <div className="card card-ct-product">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src={image20} alt="Placeholder" />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="content">
+                  <h1 className="title">Women's blackvest Gucci</h1>
+                </div>
+              </div>
+              <footer className="card-footer card-footer-ct-product">
+                <a href="#a" className="card-footer-item">
+                  <h1 className="title is-4">$715</h1>
+                </a>
+                <a href="#a" className="card-footer-item">Show now</a>
+              </footer>
+            </div>
+          </div>
+          <div className="column is-3">
+            <div className="card card-ct-product">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src={image20} alt="Placeholder" />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="content">
+                  <h1 className="title">Women's blackvest Gucci</h1>
+                </div>
+              </div>
+              <footer className="card-footer card-footer-ct-product">
+                <a href="#a" className="card-footer-item">
+                  <h1 className="title is-4">$715</h1>
+                </a>
+                <a href="#a" className="card-footer-item">Show now</a>
+              </footer>
+            </div>
+          </div>
+          <div className="column is-3">
+            <div className="card card-ct-product">
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src={image20} alt="Placeholder" />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="content">
+                  <h1 className="title">Women's blackvest Gucci</h1>
+                </div>
+              </div>
+              <footer className="card-footer card-footer-ct-product">
+                <a href="#a" className="card-footer-item">
+                  <h1 className="title is-4">$715</h1>
+                </a>
+                <a href="#a" className="card-footer-item">Show now</a>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="container">
+          <div className="columns px-6">
+            <div className="column">
+              <div className="content">
+                <h1>Clay Shop</h1>
+                <p>Fashion is a popular aesthetic expression at a particular time, place and in a specific context, especially in clothing, footwear, lifestyle, accessories, makeup.</p>
+              </div>
+            </div>
+            <div className="column">
+              <div className="content">
+                <h1>Hot links</h1>
+                <p><a href="#a">Home</a></p>
+                <p><a href="#a">Shop</a></p>
+                <p><a href="#a">Blog</a></p>
+                <p><a href="#a">Contact</a></p>
+              </div>
+            </div>
+            <div className="column">
+              <div className="content">
+                <h1>More info</h1>
+                <p>How it works</p>
+                <p>About us</p>
+                <p>Decline rules</p>
+                <p>Terms & Conditions</p>
+              </div>
+            </div>
+            <div className="column">
+              <div className="content">
+                <h1>Customer care</h1>
+                <p>FAQ</p>
+                <p>Tems of use</p>
+                <p>Private Policy</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
