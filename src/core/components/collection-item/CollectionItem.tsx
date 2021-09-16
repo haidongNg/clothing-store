9430e74b992680820e642c436835e5b0b2b1b05f
haidongNg <nguyenhaidong1412@hotmail.com>
2021-09-16 22:13:58 +0700

    update layout cart-dropdown

--- src/core/components/collection-item/CollectionItem.tsx
+++ src/core/components/collection-item/CollectionItem.tsx
@@ -11,7 +11,7 @@ const CollectionItem: FC<CollectionItemProps> = ({ item, addItem }) => {
   return (
     <div className="group relative" onClick={() => addItem(item)}>
       <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-        <img src={item.imageUrl} alt="Product" className="w-full h-full object-center lg:w-full lg:h-full" />
+        <img src={item.imageUrl} alt="Product" className="w-full h-full object-center object-cover lg:w-full lg:h-full" />
       </div>
       <div className="mt-4 flex justify-between">
         <div>
